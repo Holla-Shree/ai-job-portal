beb893d8f80475777e58d2a5d15cad89cd18a9e6
shreelakshmi bhat <shreelakshmibhat1971@gmail.com>
2025-07-21 12:46:25 +0000

    Why the Job seeker dashboard scroll is not working? Where the other two

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="flex h-screen flex-col">
       <AppHeader />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex-1 overflow-y-auto">
         {children}
       </div>
       <AppFooter />
